c235affc2a386c61428324385fa9c6d25aa17e30
f1vlad <kulchitski@gmail.com>
2019-12-08 17:45:19 -0800

    Prepare for 1.1 release

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-NPM package that returns a boolean depending whether or not precious markets are open or closed
+NPM package that returns a boolean depending whether or not precious markets are open or closed for a given time. The time provided could be current or in the future/past.
 
 Usage:
-
-- (coming soon)
+- npm install precious-metals-markets --save
+- `import * as markets from 'precious-metals-markets';`
+- `markets.marketsOpen(Date.now());`
+- `markets.marketsOpen(new Date("Wed, 27 March 2019 13:30:00"));`
--- index.js
+++ index.js
@@ -1,5 +1,45 @@
-const markets = () => {
-    return 'coming soon';
-}
+// Precious Markets Operating Hours
+// (https://www.jmbullion.com/faq/precious-metals-market-hours/)
+//
+// Monday -- closed 17:00-18:00
+// Tuesday -- closed 17:00-18:00
+// Wednesday -- closed 17:00-18:00
+// Thursday -- closed 17:00-18:00
+// Friday -- closed 17:00
+// Saturday -- closed
+// Sunday -- open 18:00
 
-module.exports = markets;
+const moment = require('moment');
+const timezone = require('moment-timezone');
+
+module.exports.marketsOpen = timestamp => {
+    let easternWeekday = moment(timezone(timestamp).tz('America/New_York')).format('d');
+    let easternHour = parseInt(moment(timezone(timestamp).tz('America/New_York')).format('HH'));
+
+    switch (easternWeekday) {
+        case "1":
+        case "2":
+        case "3":
+        case "4":
+        // Monday, Tuesday, Wednesday, Thursday -- markets closed 17:00--18:00
+            if (easternHour === 17) {
+                return false;
+            }
+            return true;
+        case "5":
+            // Friday -- markets closed after 17:00
+            if (easternHour >= 17) {
+                return false;
+            }
+            return true;
+        case "6":
+            // Saturday
+            return false;
+        case "0":
+            // Sunday -- markets open after 18:00
+            if (easternHour >= 18) {
+                return true;
+            }
+            return false;
+    }
+};
