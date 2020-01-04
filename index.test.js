c4b3a9d95ab1f730e6ccadcf0b1c03336a15fa49
f1vlad <kulchitski@gmail.com>
2020-01-03 16:42:59 -0800

    Edit test descriptions

--- index.test.js
+++ index.test.js
@@ -8,7 +8,7 @@ test("Market status for 8/7/11 14:14 PST, markets open", () => {
     expect(marketsOpen(new Date("7 August 2011 14:14:00 GMT-0800"))).toBe(true);
 });
 
-test("Market status for 1/20/18 11:00:00 PST, markets to open in 1 day", () => {
+test("Market status for 1/20/18 11:00:00 PST, markets to open in a day", () => {
     expect(marketsOpen(new Date("20 January 2018 11:00:00 GMT-0800"))).toBe('in a day');
 });
 
@@ -16,6 +16,6 @@ test("Market status for 1/6/20 14:30:00 PST, to open in 30 minutes", () => {
     expect(marketsOpen(new Date("6 January 2020 14:30:00 GMT-0800"))).toBe('in 30 minutes');
 });
 
-test("Market status for 1/6/20 14:59:00 PST, to open in 30 minutes", () => {
+test("Market status for 1/6/20 14:59:00 PST, to open in a minute", () => {
     expect(marketsOpen(new Date("6 January 2020 14:59:00 GMT-0800"))).toBe('in a minute');
 });
